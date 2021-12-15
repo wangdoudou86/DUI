5763f21acddf56f31fb17c566a1d8a725dea1edc
wangting <892184375@qq.com>
2021-12-15 17:40:31 +0800

    使用jest.fn()来测试点击事件

--- lib/icon/__tests__/icon.unit.tsx
+++ lib/icon/__tests__/icon.unit.tsx
@@ -1,10 +1,17 @@
 import * as renderer from 'react-test-renderer';
 import React from 'react';
 import Icon from '../icon';
+import {mount} from 'enzyme';
 
 describe('Icon', () => {
     it('是一个svg图标', () => {
         const json = renderer.create(<Icon name="alipay"/>).toJSON()
         expect(json).toMatchSnapshot()
+    })
+    it('点击icon', ()=>{
+        const fn = jest.fn()
+        const component = mount(<Icon name="alipay" onClick={fn}/>)
+        component.find('svg').simulate('click')   // 找到svg元素并模拟点击事件
+        expect(fn).toBeCalled()
     })
 }) 
