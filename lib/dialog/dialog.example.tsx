f2d0b438c49dba1abe6044f17c8be36387b6abdb
wangting <892184375@qq.com>
2022-01-05 20:23:23 +0800

    [Dialog]1、添加点击事件；2、可自定义按钮；

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,14 +1,41 @@
-import React, { useState } from 'react';
-import Dialog from './dialog';
+import React, { useState } from "react";
+import Dialog from "./dialog";
+import './dialog.example.scss';
 
-const DialogExample: React.FunctionComponent = ()=>{
-    const [x, setX] = useState(false)
-    return (
-        <div>
-            <button onClick={()=> setX(!x) }>hello</button>
-            <Dialog visible={x} />
-        </div>
-    )
-}
+const DialogExample: React.FunctionComponent = () => {
+  const [x, setX] = useState(false);
+  const [y, setY] = useState(false);
+  return (
+    <div>
+      <div style={{position: 'relative', zIndex: 10}}>
+        <h1>Example 1</h1>
+        <button onClick={() => setX(!x)}>hello1</button>
+        <Dialog
+          visible={x}
+          buttons={[<button>1</button>, <button>2</button>]}
+          onClose={() => {
+            setX(false);
+          }}
+        >
+          <h3>hello1</h3>
+        </Dialog>
+      </div>
+      <div>
+        <h1>Example 2</h1>
+        <button onClick={() => setY(!y)}>hello2</button>
+        <Dialog
+          visible={y}
+          buttons={[<button>1</button>, <button>2</button>]}
+          onClose={() => {
+            setY(false);
+          }}
+          closeonClickMask={true}
+        >
+          <h3>hello2</h3>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
 
 export default DialogExample;
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,32 +1,52 @@
-import React, { Fragment } from "react";
+import React, { Fragment, ReactElement } from "react";
 import Icon from "../icon/icon";
 import "./dialog.scss";
 import { scopedClassMaker } from "../utils";
+import ReactDOM from 'react-dom';
 
 interface DialogIcon {
   visible: boolean;
+  buttons: Array<ReactElement>;
+  onClose: React.MouseEventHandler; 
+  closeonClickMask?: boolean;
 }
 
 const scopedClass = scopedClassMaker("dui-dialog");
 const sc = scopedClass; // 主要为了简化名称
 
-const Dialog: React.FunctionComponent<DialogIcon> = (props) => {
-  return props.visible ? (
+const Dialog: React.FunctionComponent<DialogIcon> = props => {
+  const onClickClose: React.MouseEventHandler = e => {
+    props.onClose(e);
+  };
+
+  const onClickMask: React.MouseEventHandler = e => {
+    if(props.closeonClickMask) props.onClose(e);
+  }
+
+  const x = props.visible ? (
     <Fragment>
-      <div className={sc("mask")}></div>
+      <div className={sc("mask")} onClick={onClickMask}></div>
       <div className={sc()}>
-        <div className={sc("close")}>
+        <div className={sc("close")} onClick={onClickClose}>
           <Icon name="close" />
         </div>
         <header className={sc("header")}>提示</header>
-        <main className={sc("main")}>内容</main>
+        <main className={sc("main")}>{props.children}</main>
         <footer className={sc("footer")}>
-          <button>ok</button>
-          <button>cancle</button>
+          {props.buttons.map((button, index) => {
+            return React.cloneElement(button, { key: index });
+          })}
         </footer>
       </div>
     </Fragment>
   ) : null;
+
+  return ReactDOM.createPortal(x, document.body)
 };
+
+// 给组件的一些props设置默认值
+Dialog.defaultProps = {
+  closeonClickMask: false
+}
 
 export default Dialog;
