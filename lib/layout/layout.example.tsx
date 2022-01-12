bf5ade7ba9848a547664870f5ac708e7db91134d
wangting <892184375@qq.com>
2022-01-12 19:10:32 +0800

    【Layout】1、完善asider；2、添加样式；

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -3,16 +3,51 @@ import Layout from "./layout";
 import Header from "./header";
 import Content from "./content";
 import Footer from "./footer";
+import Asider from "./asider";
+import "./layout.example.scss";
 
 export default function () {
   return (
     <div>
       <div>
         <h1>Example 1</h1>
-        <Layout style={{ height: 300 }} className="hi">
-          <Header className="hello">Header</Header>
-          <Content className="hello22">Content</Content>
-          <Footer className="hello33">Footer</Footer>
+        <Layout>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </div>
+      <div>
+        <h1>Example 2</h1>
+        <Layout>
+          <Header>Header</Header>
+          <Layout>
+            <Asider>Asider</Asider>
+            <Content>Content</Content>
+          </Layout>
+          <Footer>Footer</Footer>
+        </Layout>
+      </div>
+      <div>
+        <h1>Example 3</h1>
+        <Layout>
+          <Header>Header</Header>
+          <Layout>
+            <Content>Content</Content>
+            <Asider>Asider</Asider>
+          </Layout>
+          <Footer>Footer</Footer>
+        </Layout>
+      </div>
+      <div>
+        <h1>Example 4</h1>
+        <Layout>
+          <Asider>Asider</Asider>
+          <Layout>
+            <Header>Header</Header>
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+          </Layout>
         </Layout>
       </div>
     </div>
--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import './layout.scss';
 import { scopedClassMaker } from "../utils";
 import classes from "../helpers/classes";
+import Asider from "./asider";
 
-interface LayoutProps extends React.HTMLAttributes<HTMLElement>{
+interface LayoutProps extends React.HTMLAttributes<HTMLElement> {
+    children: ReactElement | Array<ReactElement>;
 }
 const sc = scopedClassMaker('dui-layout')
-const Layout: React.FunctionComponent<LayoutProps> = (props)=>{
-    const {className, ...restProps} = props
-    return (<div className={classes(sc(), className)} {...restProps}>
-        {props.children} 
+const Layout: React.FunctionComponent<LayoutProps> = (props) => {
+    const children = props.children as Array<ReactElement>
+    const hasAsider = children.length > 0 && children.reduce((result, current) => {
+        return result || current.type === Asider
+    }, false)
+    const asiderClass = hasAsider ? sc('hasAsider') : undefined
+    const { className, ...restProps } = props
+    return (<div className={classes(sc(), asiderClass, className)} {...restProps}>
+        {props.children}
     </div>)
 }
-
 export default Layout;
