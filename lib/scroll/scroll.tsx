e808588e74292f93b72b79d70991472b4581ea8b
darkTi <892184375@qq.com>
2022-05-01 02:26:45 +0800

    【scroll】制作滚动条

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './scroll.scss';
 import { scopedClassMaker, classes } from '../helpers/utils';
 import scrollbarWidth from './scrollbar-width';
+import { useEffect, useRef, useState } from 'react';
 
 const scopedClass = scopedClassMaker('dui-scroll')
 const sc = scopedClass;
@@ -10,10 +11,36 @@ interface ScrollProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Scroll: React.FunctionComponent<ScrollProps> = (props) => {
     const {children, className, ...rest} = props;
+    const [barHeight, setBarHeight] = useState(0);
+    const [barTop, setBarTop] = useState(0);
+    const containerRef = useRef<HTMLDivElement>(null)
+
+    useEffect(()=>{
+        const {current} = containerRef;
+        const viewHeight = current!.getBoundingClientRect().height;
+        const scrollHeight = current!.scrollHeight;
+        setBarHeight(viewHeight * viewHeight / scrollHeight);
+    }, [])
+
+    const onScroll = ()=>{
+        const {current} = containerRef;
+        const viewHeight = current!.getBoundingClientRect().height;
+        const scrollHeight = current!.scrollHeight; 
+        const scrollTop = current!.scrollTop
+
+        setBarTop(viewHeight * scrollTop / scrollHeight)
+    }
+
     return(
         <div className={classes(sc(), className)} {...rest}>
-            <div className={sc('inner')} style={{right: -scrollbarWidth()}}>
+            <div className={sc('inner')} style={{right: -scrollbarWidth()}} 
+            ref={containerRef}
+            onScroll={onScroll}
+            >
                 {children}
+            </div>
+            <div className={sc('track')}>
+                <div className={sc('bar')} style={{height: barHeight, transform: `translateY(${barTop}px)`}}></div>
             </div>
         </div>
     )
