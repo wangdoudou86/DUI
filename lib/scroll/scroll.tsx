d6c2af4cb5f81a1314109f0c83c9efd65e88591a
darkTi <892184375@qq.com>
2022-05-02 02:22:51 +0800

    【scroll】使页面内容随拖动滚动条而滚动；

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -2,7 +2,13 @@ import * as React from "react";
 import "./scroll.scss";
 import { scopedClassMaker, classes } from "../helpers/utils";
 import scrollbarWidth from "./scrollbar-width";
-import { MouseEventHandler, UIEventHandler, useEffect, useRef, useState } from "react";
+import {
+  MouseEventHandler,
+  UIEventHandler,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 const scopedClass = scopedClassMaker("dui-scroll");
 const sc = scopedClass;
@@ -14,20 +20,19 @@ const Scroll: React.FunctionComponent<ScrollProps> = (props) => {
   const [barHeight, setBarHeight] = useState(0);
   const [barTop, _setBarTop] = useState(0);
   const containerRef = useRef<HTMLDivElement>(null);
-  
+
   //需要对鼠标拖动滚动条滑动时的距离做限制
   //滚动条可滚动的距离最小为0，最大为把bar拉到底时距离上面的高度
   //所以把它限制在这个范围即可
-  const setBarTop = (number: number)=>{
-    if(number<0) return;
+  const setBarTop = (number: number) => {
+    if (number < 0) return;
     const { current } = containerRef;
     const viewHeight = current!.getBoundingClientRect().height;
     const scrollHeight = current!.scrollHeight;
-    const maxBarTop = (scrollHeight - viewHeight) * viewHeight / scrollHeight
-    if(number > maxBarTop) return;
-    _setBarTop(number) 
-  }
-
+    const maxBarTop = ((scrollHeight - viewHeight) * viewHeight) / scrollHeight;
+    if (number > maxBarTop) return;
+    _setBarTop(number);
+  };
 
   useEffect(() => {
     const { current } = containerRef;
@@ -36,7 +41,7 @@ const Scroll: React.FunctionComponent<ScrollProps> = (props) => {
     setBarHeight((viewHeight * viewHeight) / scrollHeight);
   }, []);
 
-  const onScroll:UIEventHandler = () => {
+  const onScroll: UIEventHandler = () => {
     const { current } = containerRef;
     const viewHeight = current!.getBoundingClientRect().height;
     const scrollHeight = current!.scrollHeight;
@@ -45,28 +50,54 @@ const Scroll: React.FunctionComponent<ScrollProps> = (props) => {
     setBarTop((viewHeight * scrollTop) / scrollHeight);
   };
 
-  const isDraggingRef = useRef(false)
-  const initBarTopRef = useRef(0) //用来记录拖动bar时，当时bar离顶的距离
-  const initYRef = useRef(0) //用来记录鼠标拖动时，当下鼠标事件的y坐标轴的距离
-  const onMouseDownBar:MouseEventHandler = (e)=>{
-    isDraggingRef.current = true
-    console.log('start');
-    initYRef.current = e.clientY
-    initBarTopRef.current = barTop
-  }
-  const onMouseMoveBar:MouseEventHandler = (e)=>{
-    if(isDraggingRef.current){
-        let delta = e.clientY - initYRef.current //滑动时鼠标的y轴差值
-        let newBarTop = initBarTopRef.current + delta
-        //使用鼠标拖动滚动条
-        //setBarTop里有_setBarTop，每当_setBarTop都会render一次
-        setBarTop(newBarTop)
+  const isDraggingRef = useRef(false);
+  const initBarTopRef = useRef(0); //用来记录拖动bar时，当时bar离顶的距离
+  const initYRef = useRef(0); //用来记录鼠标拖动时，当下鼠标事件的y坐标轴的距离
+ 
+  const onMouseDownBar: MouseEventHandler = (e) => {
+    isDraggingRef.current = true;
+    initYRef.current = e.clientY;
+    initBarTopRef.current = barTop;
+  };
+
+  const onMouseMoveBar = (e: MouseEvent) => {
+    if (isDraggingRef.current) {
+      let delta = e.clientY - initYRef.current; //滑动时鼠标的y轴差值
+      let newBarTop = initBarTopRef.current + delta;
+      //使用鼠标拖动滚动条
+      //setBarTop里有_setBarTop，每当_setBarTop都会render一次
+      setBarTop(newBarTop);
+
+      const { current } = containerRef;
+      const viewHeight = current!.getBoundingClientRect().height;
+      const scrollHeight = current!.scrollHeight;
+      //使页面随拖动滚动条而滑动
+      //注意这里，不要用barTop，因为_setBarTop时是异步更新
+      //直接使用barTop有可能取不到最新的值，要用newBarTop
+      current!.scrollTop = (scrollHeight * newBarTop) / viewHeight;
     }
+  };
+
+  const onMouseUpBar = (e: MouseEvent) => {
+    isDraggingRef.current = false;
+  };
+
+  const onSelect = (e: MouseEvent)=>{
+      if(isDraggingRef.current){
+        e.preventDefault()
+      }
   }
-  const onMouseUpBar:MouseEventHandler = (e)=>{
-    isDraggingRef.current = false
-    console.log('end');
-  }
+
+  useEffect(() => {
+      document.addEventListener('mousemove', onMouseMoveBar)
+      document.addEventListener('mouseup', onMouseUpBar)
+      document.addEventListener('selectstart', onSelect)
+      return ()=>{
+        document.removeEventListener('mousemove', onMouseMoveBar)
+        document.removeEventListener('mouseup', onMouseUpBar)
+        document.removeEventListener('selectstart', onSelect)
+      }
+  }, []);
 
   return (
     <div className={classes(sc(), className)} {...rest}>
@@ -83,8 +114,6 @@ const Scroll: React.FunctionComponent<ScrollProps> = (props) => {
           className={sc("bar")}
           style={{ height: barHeight, transform: `translateY(${barTop}px)` }}
           onMouseDown={onMouseDownBar}
-          onMouseMove={onMouseMoveBar}
-          onMouseUp={onMouseUpBar}
         ></div>
       </div>
     </div>
